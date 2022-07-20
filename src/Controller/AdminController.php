4daeb5abec886e6b1fca96f80c1d9b95cd290f6c
Maarcaa <103992561+Maarcaa@users.noreply.github.com>
2022-07-20 15:58:27 +0200

    update admin + role hierarchie + access control

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller;
 
+use DateTime;
 use App\Entity\Article;
 use App\Form\ArticleFormType;
-use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\File\Exception\FileException;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\ResponseInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 /**
  * @Route("/admin")
@@ -24,6 +27,17 @@ class AdminController extends AbstractController
      */
     public function showDashboard(EntityManagerInterface $entityManager): Response
     {
+
+        // 1ere facon se trouve dan,s "access control" ->config/packages/security.yaml à décommenter role/admin
+        // 2eme facon de bloquer un acces a un user en fonction de son role
+// Ce bloc de code vous permet de vérifier si le role du user est admin, sinon cela lance une erreur qui est attrapé dans le catch et cela redirige ac un message dans une partie autorisée poiur les différents role
+try {
+    $this->denyAccessUnLessGranted('ROLE_ADMIN');
+} catch (AccessDeniedException $exception){
+$this->addFlash('warning', 'Cette parti du site est réservée aux admins');
+return $this->redirectToRoute('default_home');
+}
+
         $articles = $entityManager->getRepository(Article::class)->findBy(['deletedAt' => null]);
 
         return $this->render("admin/show_dashboard.html.twig", [
@@ -43,7 +57,7 @@ class AdminController extends AbstractController
         $form = $this->createForm(ArticleFormType::class, $article)
             ->handleRequest($request);
 
-        if($form->isSubmitted() && $form->isValid()) {
+        if ($form->isSubmitted() && $form->isValid()) {
 
             $article->setCreatedAt(new DateTime());
             $article->setUpdatedAt(new DateTime());
@@ -55,12 +69,12 @@ class AdminController extends AbstractController
             $photo = $form->get('photo')->getData();
 
             # Si une photo a été uploadée dans le formulaire on va faire le traitement nécessaire à son stockage dans notre projet.
-            if($photo) {
+            if ($photo) {
                 # Déconstructioon
                 $extension = '.' . $photo->guessExtension();
                 $originalFilename = pathinfo($photo->getClientOriginalName(), PATHINFO_FILENAME);
                 $safeFilename = $slugger->slug($originalFilename);
-//                $safeFilename = $article->getAlias();
+                //                $safeFilename = $article->getAlias();
 
                 # Reconstruction
                 $newFilename = $safeFilename . '_' . uniqid() . $extension;
@@ -68,21 +82,19 @@ class AdminController extends AbstractController
                 try {
                     $photo->move($this->getParameter('uploads_dir'), $newFilename);
                     $article->setPhoto($newFilename);
-                }
-                catch(FileException $exception) {
+                } catch (FileException $exception) {
                     # Code à exécuter en cas d'erreur.
                 }
             } # end if($photo)
 
-                # Ajout d'un auteur à l'article (User récupéré depuis la session)
-                $article->setAuthor($this->getUser());
+            # Ajout d'un auteur à l'article (User récupéré depuis la session)
+            $article->setAuthor($this->getUser());
 
-                $entityManager->persist($article);
-                $entityManager->flush();
+            $entityManager->persist($article);
+            $entityManager->flush();
 
-                $this->addFlash('success', "L'article est en ligne avec succès !");
-                return $this->redirectToRoute('show_dashboard');
-
+            $this->addFlash('success', "L'article est en ligne avec succès !");
+            return $this->redirectToRoute('show_dashboard');
         } # end if ($form)
 
         # 3 - Création de la vue
@@ -103,7 +115,7 @@ class AdminController extends AbstractController
             'photo' => $originalPhoto
         ])->handleRequest($request);
 
-        if($form->isSubmitted() && $form->isValid()) {
+        if ($form->isSubmitted() && $form->isValid()) {
 
 
             $article->setCreatedAt(new DateTime());
@@ -116,13 +128,13 @@ class AdminController extends AbstractController
             $photo = $form->get('photo')->getData();
 
             # Si une photo a été uploadée dans le formulaire on va faire le traitement nécessaire à son stockage dans notre projet.
-            if($photo) {
+            if ($photo) {
 
                 # Déconstructioon
                 $extension = '.' . $photo->guessExtension();
                 $originalFilename = pathinfo($photo->getClientOriginalName(), PATHINFO_FILENAME);
                 $safeFilename = $slugger->slug($originalFilename);
-//                $safeFilename = $article->getAlias();
+                //                $safeFilename = $article->getAlias();
 
                 # Reconstruction
                 $newFilename = $safeFilename . '_' . uniqid() . $extension;
@@ -130,8 +142,7 @@ class AdminController extends AbstractController
                 try {
                     $photo->move($this->getParameter('uploads_dir'), $newFilename);
                     $article->setPhoto($newFilename);
-                }
-                catch(FileException $exception) {
+                } catch (FileException $exception) {
                     # Code à exécuter en cas d'erreur.
                 }
             } else {
@@ -153,7 +164,7 @@ class AdminController extends AbstractController
             'form' => $form->createView(),
             'article' => $article
         ]);
-    }# end function updateArticle
+    } # end function updateArticle
 
     /**
      * @Route("/archiver-un-article_{id}", name="soft_delete_article", methods={"GET"})
@@ -167,6 +178,55 @@ class AdminController extends AbstractController
 
         $this->addFlash('success', "L'article a bien été archivé.");
         return $this->redirectToRoute('show_dashboard');
+    } # end function soft delete
+
+    /**
+     * @Route ("/restaurer-un-article_{id}", name="restore_article", methods={"GET"})
+     */
+    public function restoreArticle(Article $article, EntityManagerInterface $entityManager): RedirectResponse
+    {
+        $article->setDeletedAt(null);
+
+        $entityManager->persist($article);
+        $entityManager->flush();
+
+        $this->addFlash('success', "L'article a bien été restauré");
+        return $this->redirectToRoute('show_dashboard');
     }
 
+    /**
+     * @Route("/voir-les-articles-archives", name="show_trash", methods={"GET"})
+     */
+    public function showTrash(EntityManagerInterface $entityManager): Response
+    {
+        $archivedArticles = $entityManager->getRepository(Article::class)->findByTrash();
+
+        return $this->render("admin/trash/article_trash.html.twig", [
+            'archivedArticles' => $archivedArticles
+        ]);
+    }
+
+    /**
+     * @Route("/supprimer-un-article_{id}", name="hard_delete_article", methods={"GET"})
+     */
+    public function hardDeleteArticle(Article $article, EntityManagerInterface $entityManager): RedirectResponse
+    {
+
+        // le hardelete ne supprime que la ligne dans la bdd, il n'a pas acces au photo qui sont sur le vscode qu'il faut supprimer nous meme avec la ligne de commande suivante
+
+        // suppression manuelle de la photo
+$photo = $article->getPhoto();
+
+// on utilise la fonction native de PHP unLink() pour supprimer un fichier dans le filesystem(aussi appelé arborescence)
+if($photo){
+    unLink($this->getParameter('uploads_dir'). '/' . $photo);
+};
+
+
+        $entityManager->remove($article);
+        $entityManager->flush();
+
+        $this->addFlash('success', "l'article a bien été supprimé de la base de données");
+        return $this->redirectToRoute('show_trash');
+    }
 } # end class
--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,18 +2,24 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class DefaultController extends AbstractController
 {
     /**
      * @Route ("/", name="default_home", methods={"GET"})
      */
-   public function home(): Response
+   public function home(EntityManagerInterface $entityManager): Response
    {
-    return $this->render('default/home.html.twig');
+    # getRepository récupere toutes les données de la BDD
+$articles = $entityManager->getRepository(Article::class)->findBy(['deletedAt' => null]);
+
+    return $this->render('default/home.html.twig',[
+      'articles' => $articles
+    ]);
    }
 }
